cb41c35631c6001e02d991873d578fd64bc1875e
LemonTorrent <nyarbrough14@gmail.com>
2023-03-13 14:29:03 -0700

    Added componenets folder

---
+++ final-project/components/Navbar.js
@@ -0,0 +1,47 @@
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+
+const NavBarDiv = styled.div`
+    // background-color: #f4cce4;
+    // background-color: #d8f4dc;
+    background-color: black;
+    display: flex;
+    padding: 0;
+    width: 100%;
+    justify-content: center;
+    color: white;
+
+    li {
+        list-style-type: none;
+        height: 100%;
+    }
+
+    .navbar {
+
+    }
+    .navbar-items {
+        height: 100%;
+        // width: max-contents;
+        // width: 40px;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    a {
+        text-decoration: none;
+        color: white;
+        padding: 10px;
+        font-size: 30px;
+    }
+`
+
+export default function Navbar() {
+    return (
+        <NavBarDiv>
+            <h1>Penny Candy Store</h1>
+        </NavBarDiv>
+    )
+  }
+  
