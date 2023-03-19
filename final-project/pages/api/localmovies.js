767d25c4d3211043e947e08f81c246fad60323da
Ryan <smithr01@yahoo.com>
2023-03-19 13:12:38 -0700

    weird git bug

--- final-project/pages/api/localmovies.js
+++ final-project/pages/api/localmovies.js
@@ -1,30 +1,31 @@
 import clientPromise from '../../lib/mongo'
-
-
-
 const {ObjectId} = require('mongodb')
 const mongoDBName = process.env.MONGODB_NAME
 
+export const movieSchema = {
+    externalID: {required: true}
+}
 
 export default async function (req, res) {
     if(req.method === "POST") {
-        const returnID = await addMovieToDB(JSON.stringify(req.body))
-        res.status(201).json({id: returnID})
+        console.log("Got post request: ", req.body)
+        const returnID = await addMovie(JSON.stringify(req.body))
+        //console.log("Got return id: ", returnID)
+        if(ret)
+        res.status(201).json({id: returnID.toString()})
     }
 }
 
-async function addMovieToDB(movie) {
+async function addMovie(movie) {
     const client = await clientPromise
-    console.log("adding movie: ", movie)
     const db = client.db(mongoDBName)//getDbInstance()
     if(db){
-        console.log("db: ", db)
         const collection = await db.collection('movies')
-        const result = await collection.insertOne(JSON.parse(movie))
+        const result = await collection.insertOne(JSON.parse(test))
         return result.insertedId;
     }else{
-        console.log("Error: no database: ", db)
-        return 0
+        console.log(`Error: couldn't connect to the database '${mongoDBName}'`)
+        return -1
     }
     
 }
