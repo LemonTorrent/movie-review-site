a6e60269f38d3b5716f8d9906fbbc77e1ff2339e
Ethan Hampton <etho@ethohampton.com>
2023-03-22 10:25:48 -0700

    Add login button to header

--- final-project/components/Navbar.js
+++ final-project/components/Navbar.js
@@ -1,6 +1,9 @@
+/** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 import Link from 'next/link'
+
+import { isLoggedIn, signout } from '../lib/clientsideAuth'
 
 const NavBarDiv = styled.div`
     // background-color: #f4cce4;
@@ -38,13 +41,36 @@ const NavBarDiv = styled.div`
     }
 `
 
+const buttonLink = css`
+    text-decoration: none;
+    color: white;
+    padding: 10px;
+    font-size: 20px;
+    background-color: black;
+    border: 1px solid white;
+    border-radius: 5px;
+    margin: 10px;
+`
+
+const FixedRight = styled.div`
+    position: absolute;
+    right: 0;
+    top: 0;
+    //height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`
+
 export default function Navbar() {
     return (
         <NavBarDiv>
             <Link href={"/"}>
                 <h1>Benny's Movie Reviews</h1>
             </Link>
+            <FixedRight>
+            {isLoggedIn() ? (<button css={buttonLink} onClick={signout}>Log out</button>) : (<Link href={"/login"} css={buttonLink}>Login</Link>)}
+            </FixedRight>
         </NavBarDiv>
     )
-  }
-  
+}
